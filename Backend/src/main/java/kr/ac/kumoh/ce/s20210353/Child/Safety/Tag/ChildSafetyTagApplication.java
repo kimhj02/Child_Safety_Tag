93a83a25ed8b81f93fd32a2b9982fce760de5915
KIM Hyunjin <snowrabbit123@naver.com>
2025-06-24 23:53:28 +0900

    first commit

---
+++ Backend/src/main/java/kr/ac/kumoh/ce/s20210353/Child/Safety/Tag/ChildSafetyTagApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20210353.Child.Safety.Tag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildSafetyTagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildSafetyTagApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/kr/ac/kumoh/ce/s20210353/Child/Safety/Tag/ChildSafetyTagApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20210353.Child.Safety.Tag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildSafetyTagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
